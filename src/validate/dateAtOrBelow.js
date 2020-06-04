b29bfabeb8536ec2339fa601cf0f56f994a0e427
Breno Luz <marxluz@gmail.com>
2020-06-04 20:17:19 -0300

    ajust na validaçao

--- src/validate/dateAtOrBelow.js
+++ src/validate/dateAtOrBelow.js
@@ -1,30 +1,62 @@
 var Base = require('./base');
+const tools = require('../tools');
 
-var dateAtOrBelow = function(date){
+var dateAtOrBelow = function(date, format){
 
   this.date = date;
   this.message  = 'Data futura inválida';
+  this.format  = format;
+  this.can_transform = true;
 };
 dateAtOrBelow.prototype = new Base;
 dateAtOrBelow.prototype.constructor = dateAtOrBelow;
 module.exports = dateAtOrBelow;
 
+dateAtOrBelow.prototype.disable_transform = function(){
+
+  this.can_transform = false;
+}
+
 dateAtOrBelow.prototype.isValid = function(value, cb){
 
-  if(value instanceof Date && value.toString() == 'Invalid Date'){
-    cb(true);
-    return;
-  }
+  let is_valid = this.isValidSync(value);
+  return Promise.resolve(is_valid);
+};
+
+dateAtOrBelow.prototype.isValidSync = function(value){
 
   if(!value){
-    cb(true);
-    return;
+    return true; 
   }
 
-  var value = value instanceof Date ? value : new Date(value.split('-'));
-  if(value.today().getTime() > this.date.today().getTime()) return cb(false);
-  cb(true);
-};
+  if(!this.format){
+    this.format = tools.get_date_format();
+  }
+
+  if(!(value instanceof Date)){
+
+    if(value.length != this.format.length){
+      return false;
+    }
+  
+    if(this.can_transform){
+      value = tools.transform_date(value, this.format, 'aaaa/MM/dd');
+    }
+
+    value = new Date(value);
+  }
+
+  //não é tarefa dessa validação verificar a formatação
+  if(value.toString() != 'Invalid Date'){
+    return true;
+  }
+
+  if(value.today().getTime() > this.date.today().getTime()){
+    return false;
+  }
+
+  return true;
+}
 
 /* Params ex: [-365] => 1 year ago or below
               [01/01/2018] => 01/01/2018 or below
